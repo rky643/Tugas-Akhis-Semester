a99b1a786e74bb9fe71888c1288f609985183c78
rky643 <aldo06matulessy@gmail.com>
2024-05-15 15:23:24 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+const menu=document.querySelector(".sosmed");
+const toggle=document.querySelector(".toggle");
+toggle.addEventListener("click",()=>{
+  menu.classList.toggle("active");
+})
+
+var isActive = false;
+
+function moveContainer() {
+    var container = document.querySelector('.sosmed');
+    if (!isActive) {
+        container.style.transform = "translate(-20px, -20px)";
+        isActive = true;
+    } else {
+        container.style.transform = "translate(0, 0)";
+        isActive = false;
+    }
+}
